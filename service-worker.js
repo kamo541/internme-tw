aaf07939cdd49406dbdb792da08076686e3f420e
kamo541 <kamokerabetswe@gmail.com>
2025-06-03 02:34:28 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,20 @@
-const cacheName = 'internme-v1';
-const assets = [
-  './',
-  './index.html',
-  './js/your_script.js',
-  './manifest.json'
+const CACHE_NAME = 'internme-cache-v1';
+const urlsToCache = [
+  'index.html',
+  'confirm.html',
+  'duration.html',
+  'select-course.html',
+  'select-province.html',
+  'locations.html',
+  'data.js',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
-});
+self.addEventListener('install', event =>
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)))
+);
+self.addEventListener('fetch', event =>
+  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)))
+);
