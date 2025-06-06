962fc3dd70782cad8cd1c7613eb8348530fa525f
kamo541 <kamokerabetswe@gmail.com>
2025-06-06 09:43:47 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,46 +1,11 @@
-const internshipData = {
-  "Gauteng": {
-    "Johannesburg": {
-      "African Languages": [
-        "https://www.google.com/maps/search/Translation+in+Johannesburg,+Gauteng",
-        "https://www.google.com/maps/search/Cultural+Heritage+in+Johannesburg,+Gauteng",
-        "https://www.google.com/maps/search/Linguistics+in+Johannesburg,+Gauteng"
-      ],
-      "English": [
-        "https://www.google.com/maps/search/Literary+Studies+in+Johannesburg,+Gauteng",
-        "https://www.google.com/maps/search/Creative+Writing+in+Johannesburg,+Gauteng",
-        "https://www.google.com/maps/search/Publishing+in+Johannesburg,+Gauteng"
-      ]
-    },
-    "Pretoria": {
-      "African Languages": [
-        "https://www.google.com/maps/search/Translation+in+Pretoria,+Gauteng",
-        "https://www.google.com/maps/search/Cultural+Heritage+in+Pretoria,+Gauteng",
-        "https://www.google.com/maps/search/Linguistics+in+Pretoria,+Gauteng"
-      ]
-    }
-  },
-  "Western Cape": {
-    "Cape Town": {
-      "Afrikaans": [
-        "https://www.google.com/maps/search/Translation+in+Cape+Town,+Western+Cape",
-        "https://www.google.com/maps/search/Language+Studies+in+Cape+Town,+Western+Cape",
-        "https://www.google.com/maps/search/Education+in+Cape+Town,+Western+Cape"
-      ],
-      "English": [
-        "https://www.google.com/maps/search/Literary+Studies+in+Cape+Town,+Western+Cape",
-        "https://www.google.com/maps/search/Creative+Writing+in+Cape+Town,+Western+Cape",
-        "https://www.google.com/maps/search/Publishing+in+Cape+Town,+Western+Cape"
-      ]
-    }
-  },
-  "Northern Cape": {
-    "Kimberley": {
-      "Human Settlements Management": [
-        "https://www.google.com/maps/search/Housing+Development+in+Kimberley,+Northern+Cape",
-        "https://www.google.com/maps/search/Urban+Development+in+Kimberley,+Northern+Cape",
-        "https://www.google.com/maps/search/Public+Administration+in+Kimberley,+Northern+Cape"
-      ]
-    }
-  }
+const courseSectors = {
+  "Business Management": ["Retail", "E-commerce", "Manufacturing", "Government Agencies"],
+  "Computer Science": ["Software Development", "IT Services", "Cybersecurity"],
+  "Medical Secretary": ["Hospitals", "Clinics", "Health Departments"]
+};
+
+const provinceTowns = {
+  "Gauteng": ["Johannesburg", "Pretoria", "Midrand"],
+  "Western Cape": ["Cape Town", "Stellenbosch"],
+  "KwaZulu-Natal": ["Durban", "Pietermaritzburg"]
 };
