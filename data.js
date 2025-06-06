0f4371ae714086a7f49ebccb6ebe2b29e5f93a42
kamo541 <kamokerabetswe@gmail.com>
2025-06-06 02:07:16 +0200

    Update data.js

---
+++ data.js
@@ -0,0 +1,28 @@
+2
+const internshipData = {
+  "Computer Science": {
+    "Gauteng": {
+      "Johannesburg": [
+        {
+          industry: "Software Development",
+          link: "https://maps.app.goo.gl/your-link-1"
+        },
+        {
+          industry: "Cybersecurity",
+          link: "https://maps.app.goo.gl/your-link-2"
+        }
+      ]
+    },
+    "Pretoria": {
+      "Software Engineering": [
+        {
+          industry: "Web Development",
+          link: "https://maps.app.goo.gl/your-link-3"
+        }
+      ]
+    }
+  },
+  // Add more courses and provinces here
+};
+
+export default internshipData;
